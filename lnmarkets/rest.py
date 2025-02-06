d97cfe550a2b249ca771fd6e4cec280fe97c40e5
TonyMJ <tonymj@proton.me>
2025-02-06 09:58:10 +0100

    feat. add get_ohcls method

--- lnmarkets/rest.py
+++ lnmarkets/rest.py
@@ -217,8 +217,7 @@ class LNMarketsRest():
         params = {}
 
         return self.before_request_api(method, path, params, credentials)
-    
-    
+
     def futures_get_trade(self, params):
         method = 'GET'
         trade_id = params.get('id')  
@@ -227,6 +226,12 @@ class LNMarketsRest():
 
         return self.before_request_api(method, path, params, credentials)
 
+     def futures_get_ohlcs(self):
+        method = 'GET'
+        path = '/futures/ohlcs'
+        credentials = False
+
+        return self.before_request_api(method, path, params, credentials)
     
 
 
